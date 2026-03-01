4982ae24bf76f84bd73c64ce683a9d1471dfc1ba
shirleylima-hue <shirley.lima@uab.edu.bo>
2026-03-01 11:04:44 -0400

    Agregar módulo de eliminar por ID

--- formularios.py
+++ formularios.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Optional
 
-class FormAgregarTareas (FlaskForm):
+
+class FormAgregarTareas(FlaskForm):
     titulo = StringField('Titulo', validators=[DataRequired()])
     enviar = SubmitField('Enviar')
+
+# Eliminar por ID
+class FormEliminarTarea(FlaskForm):
+    id_tarea = IntegerField('ID a eliminar', validators=[Optional()])
+    eliminar = SubmitField('Eliminar')
