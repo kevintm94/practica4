2b9561d54071f80b3ecb9e9714fd44279bdf1d10
Veliz10 <velizugarteraul@gmail.com>
2026-03-01 23:40:00 -0400

    Funcionalidad mostrar

--- formularios.py
+++ formularios.py
@@ -1,7 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Optional
 
-class FormAgregarTareas (FlaskForm):
+
+class FormAgregarTareas(FlaskForm):
     titulo = StringField('Titulo', validators=[DataRequired()])
     enviar = SubmitField('Enviar')
+
+class FormEditarTareas (FlaskForm):
+    titulo = StringField('Titulo', validators=[DataRequired()])
+    actualizar = SubmitField('Actualizar')
+# Eliminar por ID
+class FormEliminarTarea(FlaskForm):
+    id_tarea = IntegerField('ID a eliminar', validators=[Optional()])
+    eliminar = SubmitField('Eliminar')
--- routes.py
+++ routes.py
@@ -1,29 +1,70 @@
 from app import app, db
-from flask import render_template
+from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect
 import formularios
 from models import Tarea
-
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', subtitulo = "Actidad en grupo TAI")
+    return render_template('index.html', subtitulo="Actividad en grupo TAI")
 
-@app.route('/sobrenosotros', methods = ['GET', 'POST'])
+@app.route('/sobrenosotros', methods=['GET', 'POST'])
 def sobrenosotros():
-    formulario = formularios.FormAgregarTareas()
-    if formulario.validate_on_submit() :
-        nueva_tarea = Tarea(titulo = formulario.titulo.data)
+    form_agregar = formularios.FormAgregarTareas()
+    form_eliminar = formularios.FormEliminarTarea()
+    tareas = Tarea.query.all()
+
+    # agregar nueva tarea
+    if form_agregar.validate_on_submit() and form_agregar.enviar.data:
+        nueva_tarea = Tarea(titulo=form_agregar.titulo.data)
         db.session.add(nueva_tarea)
         db.session.commit()
-        print('Se envio correctamente', formulario.titulo.data)
-        return render_template('sobrenosotros.html', form = formulario, titulo = formulario.titulo.data)
-    return render_template('sobrenosotros.html', form = formulario)
+        print('Se envio correctamente', form_agregar.titulo.data)
+        return redirect(url_for('sobrenosotros'))
+
+    # eliminar por id
+    if form_eliminar.validate_on_submit() and form_eliminar.eliminar.data:
+        tarea = Tarea.query.get(form_eliminar.id_tarea.data)
+        if tarea:
+            db.session.delete(tarea)
+            db.session.commit()
+            print('Tarea eliminada correctamente (desde formulario)')
+        return redirect(url_for('sobrenosotros'))
+
+    return render_template(
+        'sobrenosotros.html',
+        form_agregar=form_agregar,
+        form_eliminar=form_eliminar,
+        tareas=tareas,
+    )
+@app.route('/editar_tarea/<int:id>', methods=['GET', 'POST'])
+def editar_tarea(id):
+    tarea = Tarea.query.get_or_404(id)
+    formulario = formularios.FormEditarTareas()
+    
+    if formulario.validate_on_submit():
+        tarea.titulo = formulario.titulo.data
+        db.session.commit()
+        print('Tarea actualizada correctamente')
+        return redirect(url_for('sobrenosotros'))
+    elif request.method == 'GET':
+        formulario.titulo.data = tarea.titulo
+    
+    return render_template('editar_tarea.html', form = formulario, tarea = tarea)
+
+@app.route('/eliminar_tarea/<int:id>')
+def eliminar_tarea(id):
+    tarea = Tarea.query.get_or_404(id)
+    db.session.delete(tarea)
+    db.session.commit()
+    print('Tarea eliminada correctamente')
+    return redirect(url_for('sobrenosotros'))
     
 @app.route('/saludo')
 def saludo():
-    return 'Hola bienvenido a Taller Apps '
-    
+    return 'Hola bienvenido a Taller Apps'
+
 @app.route('/usuario/<nombre>')
 def usuario(nombre):
-    return f'Hola{nombre} bienvenido a Taller Apps '
+    return f'Hola {nombre} bienvenido a Taller Apps'
